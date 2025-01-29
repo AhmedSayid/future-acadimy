2eda427b4b041a3e6fc82b446abb9e7cb819e2a1
ItsMountain74 <omarsobhy@aait.cloud>
2025-01-29 17:36:04 +0200

    Updates

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verify.device' => \App\Http\Middleware\VerifyDeviceToken::class,
+        'checkRole' => \App\Http\Middleware\checkRole::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -39,61 +39,63 @@ Route::middleware(['auth'])->prefix('platform/')->group(function (){
     Route::get('/home' , [DashboardController::class ,'platform'])->name('platform.index');
     Route::get('/read-notification',[DashboardController::class,'readNotification'])->name('notifications.mark-all');
 
-    Route::controller(UserController::class)->as('teachers.')->prefix('teachers/')->group(function (){
-        Route::get('' , 'index')->name('index');
-        Route::get('change-status', 'changeBlockStatus')->name('change-status');
-        Route::get('get-teacher-data/{id}', 'getTeacherData')->name('get-teacher-data');
-        Route::post('store' , 'store')->name('store');
-        Route::get('show/{id}' , 'show')->name('show');
-        Route::post('update/{id}' , 'update')->name('update');
-        Route::delete('delete/{id}' , 'delete')->name('delete');
-    });
+    Route::middleware(['checkRole'])->group(function (){
+        Route::controller(UserController::class)->as('teachers.')->prefix('teachers/')->group(function (){
+            Route::get('' , 'index')->name('index');
+            Route::get('change-status', 'changeBlockStatus')->name('change-status');
+            Route::get('get-teacher-data/{id}', 'getTeacherData')->name('get-teacher-data');
+            Route::post('store' , 'store')->name('store');
+            Route::get('show/{id}' , 'show')->name('show');
+            Route::post('update/{id}' , 'update')->name('update');
+            Route::delete('delete/{id}' , 'delete')->name('delete');
+        });
 
-    Route::controller(ErrorLogController::class)->as('errors.')->prefix('errors')->group(function (){
-       Route::get('/','index')->name('index');
-       Route::get('/get-message/{id}','getMessage')->name('get-error-msg');
-    });
+        Route::controller(ErrorLogController::class)->as('errors.')->prefix('errors')->group(function (){
+            Route::get('/','index')->name('index');
+            Route::get('/get-message/{id}','getMessage')->name('get-error-msg');
+        });
 
-    Route::controller(GradeController::class)->as('grades.')->prefix('grades/')->group(function (){
-        Route::get('' , 'index')->name('index');
-        Route::get('get-grade-data/{id}', 'getGradeData')->name('get-grade-data');
-        Route::post('store' , 'store')->name('store');
-        Route::post('update/{id}' , 'update')->name('update');
-        Route::delete('delete/{id}' , 'delete')->name('delete');
-    });
-    Route::controller(SubjectController::class)->as('subjects.')->prefix('subjects/')->group(function (){
-        Route::get('' , 'index')->name('index');
-        Route::get('get-subject-data/{id}', 'getSubjectData')->name('get-subject-data');
-        Route::post('store' , 'store')->name('store');
-        Route::post('update/{id}' , 'update')->name('update');
-        Route::delete('delete/{id}' , 'delete')->name('delete');
-        Route::get('/get-subjects/{grade_id}', 'getSubjects')->name('getSubjects');
-    });
-    Route::controller(ChapterController::class)->as('chapters.')->prefix('chapters/')->group(function (){
-        Route::get('/{id}' ,'index')->name('index');
-        Route::get('get-chapter-data/{id}', 'getChapterData')->name('get-chapter-data');
-        Route::post('store' ,'store')->name('store');
-        Route::post('update/{id}' ,'update')->name('update');
-        Route::delete('delete/{id}','delete')->name('delete');
-    });
-    Route::controller(StudentController::class)->as('students.')->prefix('students/')->group(function (){
-        Route::get('' , 'index')->name('index');
-        Route::get('change-status', 'changeBlockStatus')->name('change-status');
-        Route::get('get-student-data/{id}', 'getStudentData')->name('get-student-data');
-        Route::post('store' , 'addStudent')->name('store');
-        Route::get('show/{id}/{notification_id?}' , 'show')->name('show');
-        Route::post('update/{id}' , 'update')->name('update');
-        Route::delete('delete/{id}' , 'delete')->name('delete');
-    });
-    Route::controller(CourseController::class)->as('courses.')->prefix('courses/')->group(function (){
-        Route::get('{id}' , 'index')->name('index');
-        Route::get('create/{id}','create')->name('create');
-        Route::post('store' , 'store')->name('store');
-        Route::post('upload-video', 'uploadVideo')->name('upload-video');
-        Route::post('show' , 'show')->name('show');
-        Route::get('edit/{id}', 'edit')->name('edit');
-        Route::post('update/{id}' , 'update')->name('update');
-        Route::delete('delete/{id}' , 'delete')->name('delete');
+        Route::controller(GradeController::class)->as('grades.')->prefix('grades/')->group(function (){
+            Route::get('' , 'index')->name('index');
+            Route::get('get-grade-data/{id}', 'getGradeData')->name('get-grade-data');
+            Route::post('store' , 'store')->name('store');
+            Route::post('update/{id}' , 'update')->name('update');
+            Route::delete('delete/{id}' , 'delete')->name('delete');
+        });
+        Route::controller(SubjectController::class)->as('subjects.')->prefix('subjects/')->group(function (){
+            Route::get('' , 'index')->name('index');
+            Route::get('get-subject-data/{id}', 'getSubjectData')->name('get-subject-data');
+            Route::post('store' , 'store')->name('store');
+            Route::post('update/{id}' , 'update')->name('update');
+            Route::delete('delete/{id}' , 'delete')->name('delete');
+            Route::get('/get-subjects/{grade_id}', 'getSubjects')->name('getSubjects');
+        });
+        Route::controller(ChapterController::class)->as('chapters.')->prefix('chapters/')->group(function (){
+            Route::get('/{id}' ,'index')->name('index');
+            Route::get('get-chapter-data/{id}', 'getChapterData')->name('get-chapter-data');
+            Route::post('store' ,'store')->name('store');
+            Route::post('update/{id}' ,'update')->name('update');
+            Route::delete('delete/{id}','delete')->name('delete');
+        });
+        Route::controller(StudentController::class)->as('students.')->prefix('students/')->group(function (){
+            Route::get('' , 'index')->name('index');
+            Route::get('change-status', 'changeBlockStatus')->name('change-status');
+            Route::get('get-student-data/{id}', 'getStudentData')->name('get-student-data');
+            Route::post('store' , 'addStudent')->name('store');
+            Route::get('show/{id}/{notification_id?}' , 'show')->name('show');
+            Route::post('update/{id}' , 'update')->name('update');
+            Route::delete('delete/{id}' , 'delete')->name('delete');
+        });
+        Route::controller(CourseController::class)->as('courses.')->prefix('courses/')->group(function (){
+            Route::get('{id}' , 'index')->name('index');
+            Route::get('create/{id}','create')->name('create');
+            Route::post('store' , 'store')->name('store');
+            Route::post('upload-video', 'uploadVideo')->name('upload-video');
+            Route::post('show' , 'show')->name('show');
+            Route::get('edit/{id}', 'edit')->name('edit');
+            Route::post('update/{id}' , 'update')->name('update');
+            Route::delete('delete/{id}' , 'delete')->name('delete');
+        });
     });
 //    Route::controller(BankQuestionController::class)->as('questions.')->prefix('questions/')->group(function (){
 //        Route::get('{id}' , 'index')->name('index');
