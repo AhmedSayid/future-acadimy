33fac5c17c1677a27ca67fcb88f0f6a7088cf68a
ItsMountain74 <omarsobhy@aait.cloud>
2025-02-03 11:44:56 +0200

    test logout

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verify.device' => \App\Http\Middleware\VerifyDeviceToken::class,
+        'verifyApi.device' => \App\Http\Middleware\VerifyDeviceToken::class,
         'checkRole' => \App\Http\Middleware\CheckRole::class,
     ];
 }
---
+++ app/Http/Middleware/verifyDeviceTokenApi.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class verifyDeviceTokenApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $sessionToken = session('device_token');
+
+            if ($user->device_token != $sessionToken) {
+                Auth::logout();
+                return [
+                    'key'   => 'failed',
+                    'msg'   => 'session expired',
+                ];
+            }
+        }
+
+        return $next($request);
+    }
+}
