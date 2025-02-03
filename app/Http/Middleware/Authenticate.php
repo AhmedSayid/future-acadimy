5d0333fd300f8e2ff8a3aa3956aa57399b850cc3
ItsMountain74 <omarsobhy@aait.cloud>
2025-02-04 01:03:12 +0200

    test fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,22 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
-    {
-        return $request->expectsJson() ? null : route('login');
-    }
-
-    protected function unauthenticated($request, array $guards)
-    {
-        throw new AuthenticationException();
+    protected function redirectTo($request) {
+        if (!$request->is('api/*')) {
+            return route('login');
+        }
     }
 }
