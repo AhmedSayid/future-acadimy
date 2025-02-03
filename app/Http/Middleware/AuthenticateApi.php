f7f382979534246a5e0d2a407210fcde1a187201
ItsMountain74 <omarsobhy@aait.cloud>
2025-02-04 00:47:48 +0200

    test fix

--- app/Http/Middleware/AuthenticateApi.php
+++ app/Http/Middleware/AuthenticateApi.php
@@ -5,13 +5,17 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+class AuthenticateApi extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request)
     {
-        return $request->expectsJson() ? null : route('login');
+        return [
+            'code'  => 401,
+            'key'   => 'fail',
+            'msg'   => 'please login again',
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('login',[AuthController::class ,'login']);
-Route::middleware(['auth:sanctum','verifyApi.device','check.block'])->group(function (){
+Route::middleware(['authApi','verifyApi.device','check.block'])->group(function (){
     Route::get('get-subjects',[SubjectController::class,'getSubjects']);
     Route::get('get-chapters/{id}',[SubjectController::class,'getChapters']);
     Route::get('get-videos/{subject_id}/{id?}',[SubjectController::class,'getVideos']);
