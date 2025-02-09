bf626c96a1076c2e6f1674bb9f6129325fb3e99d
ItsMountain74 <omarsobhy@aait.cloud>
2025-02-09 10:41:36 +0200

    try job

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -147,7 +147,7 @@ class CourseController extends Controller
                 $file = $request->file('video');
                 $filename = time() . '_' . str_replace(' ', '_', $file->getClientOriginalName());
 
-                ProcessVideoUpload::dispatchAfterResponse($file, $filename); // Send to Queue
+                ProcessVideoUpload::dispatch($file, $filename)->afterResponse(); // Send to Queue
                 dd(Storage::url(asset($filename)));
                 return response()->json(['success' => true, 'message' => 'Video is being processed in the background.','filepath' => Storage::url(asset($filename))]);
             }
